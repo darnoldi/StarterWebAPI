6ab34b18d025822eb8437bfd727d64dcfc90cf97
Dave Arnoldi <dave@arnoldi.co.za>
2017-07-29 18:24:28 +0200

    end chapter 2

--- packt_webapp/Startup.cs
+++ packt_webapp/Startup.cs
@@ -7,15 +7,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using packt_webapp.Middleware;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Diagnostics;
 
 namespace packt_webapp
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+           // Debug.WriteLine($" ---> From Config: {Configuration["firstname"]}");
+            //Debug.WriteLine($" ---> From Config: {Configuration["withChild:option1"]}");
+        }
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            services.Configure<MyConfiguration>(Configuration);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +49,17 @@ namespace packt_webapp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCustomMiddleware();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            
+
+            app.UseMvc();
+            
+           
         }
     }
 }
